cb81eb37dadaf6121907334cfe919d884e29fa1a
eri97 <eribilcari2@gmail.com>
2026-04-20 14:30:41 +0200

    updated global css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import {HomeHeroSection} from "@/components/HomeHeroSection";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <main className = "min-h-screen bg-background p-3 sm:p-4 tg:p-5">
+      <HomeHeroSection/>
+    </main>
   );
 }
