2195132c67a7f4d947928a78489fe14d59ffb0a5
eri97 <eribilcari2@gmail.com>
2026-04-20 17:59:27 +0200

    API routes

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://03fadfbb9e00623b309d9ee44dae323c@o4511248496918528.ingest.de.sentry.io/4511248503275600",
+  dsn: "https://6bc73bbd96eefae76dd6c0f3da5e3cfb@o4509813037137920.ingest.de.sentry.io/4511102283284560",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -16,4 +16,6 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+
+  integrations: [Sentry.vercelAIIntegration()],
 });
